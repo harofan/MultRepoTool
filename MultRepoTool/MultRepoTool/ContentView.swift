6175ae9f6f5313baf371175c36cb6c105053b9dd
harofan <harofan@tencent.com>
2021-09-25 17:31:14 +0800

    feat: 初始化项目

---
+++ MultRepoTool/MultRepoTool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultRepoTool
+//
+//  Created by harofan on 2021/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultRepoTool/MultRepoTool/MultRepoToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultRepoToolApp.swift
+//  MultRepoTool
+//
+//  Created by harofan on 2021/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MultRepoToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
