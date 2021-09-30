784d2e888d9827848c5a1fc887d003ac31504f8b
harofan <harofan@tencent.com>
2021-09-30 21:11:51 +0800

    feat: 引入swiftshell

--- MultRepoTool/MultRepoTool/WorkFlow/Git/GitTool.swift
+++ MultRepoTool/MultRepoTool/WorkFlow/Git/GitTool.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftGit2
+import SwiftShell
 
 final class GitTool {
     lazy var repo: Repository = try! Repository.at(URL(string: sourceDirectory)!).get()
@@ -34,14 +35,14 @@ final class GitTool {
     func pull(_ branch: String? = nil) {
         // TODO: 暂时仅支持rebase-pull的方式
         // TODO: SwiftGit2暂时不支持pull && push, 可以考虑日后业务流程完善, 踩过坑后给他提个pr
-        let result = shell(["git pull"])
+        let result = run(binPath, "git", "pull")
         print(result)
     }
     
     /// 推送远端
     /// - Parameter branch: 分支名, 不传的话为当前分支
     func push(_ branch: String? = nil) {
-        let result = shell(["git push"])
+        let result = run(binPath, "git", "push")
         print(result)
     }
     
