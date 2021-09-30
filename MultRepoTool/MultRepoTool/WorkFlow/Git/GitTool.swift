2eea3a62328c2426adce5c95de748036fb9c6994
harofan <harofan@tencent.com>
2021-09-30 21:14:10 +0800

    feat: 调整打印

--- MultRepoTool/MultRepoTool/WorkFlow/Git/GitTool.swift
+++ MultRepoTool/MultRepoTool/WorkFlow/Git/GitTool.swift
@@ -36,14 +36,14 @@ final class GitTool {
         // TODO: 暂时仅支持rebase-pull的方式
         // TODO: SwiftGit2暂时不支持pull && push, 可以考虑日后业务流程完善, 踩过坑后给他提个pr
         let result = run(binPath, "git", "pull")
-        print(result)
+        print(result.stdout)
     }
     
     /// 推送远端
     /// - Parameter branch: 分支名, 不传的话为当前分支
     func push(_ branch: String? = nil) {
         let result = run(binPath, "git", "push")
-        print(result)
+        print(result.stdout)
     }
     
     /// 计算OId
